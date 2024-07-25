051daeb3294d690041e457912cfb72fce51c89d2
Ahmed Osman <engineer.ahmedfarouk@gmail.com>
2024-07-25 08:39:40 -0300

    add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name="Medical AI ChatBot",
+    version="0.0.0",
+    author="Ahmed Osman",
+    author_email="engineer.ahmedfarouk@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
